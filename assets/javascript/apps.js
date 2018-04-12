338617d98042b4bfe54fbfd54f96b308d3031f95
NGOC NGUYEN <ngocnguyen@NGOCsMacPro-234.local>
2018-04-12 00:24:24 -0700

    added google signOut function

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -41,4 +41,12 @@ var config = {
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   }
 
+  //SignOut
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+
 /************** End of Google Authencation ****************/
