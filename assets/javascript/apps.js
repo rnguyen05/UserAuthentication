d4837d667959e5561463560f000d9e6055b853d3
NGOC NGUYEN <ngocnguyen@NGOCsMacPro-234.local>
2018-04-12 00:04:06 -0700

    console log user

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -18,7 +18,8 @@ var config = {
     var token = result.credential.accessToken;
     // The signed-in user info.
     var user = result.user;
-    // ...
+    
+    console.log(user);
   }).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
