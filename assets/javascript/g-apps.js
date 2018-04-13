084872d89edab7a90f0637bd5f8e80f8eadae673
NGOC NGUYEN <ngocnguyen@NGOCsMacPro-234.local>
2018-04-12 18:59:12 -0700

    Added username and password login

--- assets/javascript/g-apps.js
+++ assets/javascript/g-apps.js
@@ -32,41 +32,46 @@ var userImage = "";
   var fProvider = new firebase.auth.FacebookAuthProvider();
 
   //Start Google Code Here
-  firebase.auth().signInWithPopup(gProvider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-
-    userName = user.displayName;
-    showUserName(userName);
+  $("#googleBtn").on('click', function () {
+    firebase.auth().signInWithPopup(gProvider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
   
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
+      userName = user.displayName;
+      showUserName(userName);
+    
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+    //Get profile info
+    function onSignIn(googleUser) {
+      var profile = googleUser.getBasicProfile();
+      // var auth2 = gapi.auth2.getAuthInstance().signOut();
+      //auth2.disconnect();
+  
+      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+      console.log('Name: ' + profile.getName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present. 
+    }
   });
-  //Get profile info
-  function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    var auth2 = gapi.auth2.getAuthInstance().signOut();
-    //auth2.disconnect();
-
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present. 
-  }
+  
   //End of Google Code 
 
 
 
 /******************** */
+
+
 // This is called with the results from from FB.getLoginStatus().
 function statusChangeCallback(response) {
   console.log('statusChangeCallback');
